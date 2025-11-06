56ae44e6a11bb22f193a64aa501c66f3ae567a80
Mohamed Abo Full <mrabofull@gmail.com>
2025-11-06 09:25:47 +0200

    t

--- server/src/auth/services/mail.service.ts
+++ server/src/auth/services/mail.service.ts
@@ -21,14 +21,14 @@ export class MailService {
       this.transporter = nodemailer.createTransport({
         host: process.env.SMTP_HOST,
         port: parseInt(process.env.SMTP_PORT || '587'),
-        secure: false, // true for 465, false for other ports
+        secure: true, // true for 465, false for other ports
         auth: {
           user: process.env.SMTP_USER,
           pass: process.env.SMTP_PASSWORD,
         },
-        tls: {
-          rejectUnauthorized: false,
-        },
+        // tls: {
+        //   rejectUnauthorized: false,
+        // },
       });
     }
   }
