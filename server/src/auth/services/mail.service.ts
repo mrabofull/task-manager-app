d3695101c2acff48d596ab312728fad3bba726d6
Mohamed Abo Full <mrabofull@gmail.com>
2025-11-06 09:00:26 +0200

    Update mail.service

--- server/src/auth/services/mail.service.ts
+++ server/src/auth/services/mail.service.ts
@@ -26,6 +26,9 @@ export class MailService {
           user: process.env.SMTP_USER,
           pass: process.env.SMTP_PASSWORD,
         },
+        tls: {
+          rejectUnauthorized: false,
+        },
       });
     }
   }
